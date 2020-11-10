3625e58a61250ea65be1d235ec93e6732dc0494c
githubsgeek <1053374792@qq.com>
2020-11-11 00:54:44 +0800

    travistest01

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:githubsgeek/VuePressBlog.git master:main
-git push -f git@github.com:githubsgeek/VuePressBlog.git master:gh-pages
+git push -f git@github.com:githubsgeek/VuePressBlog.git master:main
 
 cd -
